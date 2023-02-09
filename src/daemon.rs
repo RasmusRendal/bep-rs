68f66ede22affc7e972e3412ba480f7317bec1ee
Rasmus Rendal <rasmus@rend.al>
2023-02-09 18:13:29 +0100

    Allow setting server host, don't panic on connection failure

--- src/daemon.rs
+++ src/daemon.rs
@@ -45,9 +45,34 @@ fn run_server(address: String) -> Result<i32, Box<dyn Error>> {
 
 }
 
+fn connect_to_server(addr: String) -> Result<u8, Box<dyn Error>> {
+    let mut stream = TcpStream::connect(addr.parse()?)?;
+    let mut poll = Poll::new()?;
+    let mut events = Events::with_capacity(128);
+
+    poll.registry().register(&mut stream, CLIENT, Interest::READABLE)?;
+    poll.poll(&mut events, Some(time::Duration::from_millis(100)))?;
+    for event in events.iter() {
+        if event.token() == CLIENT {
+            let mut buf : [u8; 8] = [0;8];
+            stream.read(&mut buf)?;
+            match str::from_utf8(&buf) {
+                Ok(v) => println!("Received data: {}", v),
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+            };
+
+        }
+    }
+    Ok(1)
+}
+
 impl Daemon {
     pub fn new(state: BepState) -> Self {
         Daemon { bind_address: Some("0.0.0.0:21027".to_string()), state: state }
+    }
+
+    pub fn set_address(&mut self, new_addr: String) {
+        self.bind_address = Some(new_addr);
     }
 
     pub fn run(&mut self) -> Result<i32, Box<dyn Error>> {
@@ -63,24 +88,7 @@ impl Daemon {
         loop {
             for peer in self.state.get_peers() {
                 for addr in self.state.get_addresses(peer) {
-                    println!("Trying to connect to {}", addr);
-                    let mut stream = TcpStream::connect(addr.parse()?)?;
-                    let mut poll = Poll::new()?;
-                    let mut events = Events::with_capacity(128);
-
-                    poll.registry().register(&mut stream, CLIENT, Interest::READABLE)?;
-                    poll.poll(&mut events, Some(time::Duration::from_millis(100)))?;
-                    for event in events.iter() {
-                        if event.token() == CLIENT {
-                            let mut buf : [u8; 8] = [0;8];
-                            stream.read(&mut buf)?;
-                            match str::from_utf8(&buf) {
-                                Ok(v) => println!("Received data: {}", v),
-                                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-                            };
-
-                        }
-                    }
+                    connect_to_server(addr);
                 }
             }
             thread::sleep(time::Duration::from_millis(1000));
