8c0ff1a6d8283d4bda3f4d0e623e3bba567e7804
Rasmus Rendal <rasmus@rend.al>
2024-12-06 11:51:25 +0100

    Update flake from monorepo

--- flake.nix
+++ flake.nix
@@ -1,24 +1,44 @@
 {
-  description = "A cool library for interacting with the Block Exchange Protocol";
+  description = "A CLI for BeerCan";
 
   inputs = {
     nixpkgs.url = "nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
+    devenv.url = "github:cachix/devenv/v0.6.3";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
-
-  outputs = {
+outputs = {
     self,
     nixpkgs,
     flake-utils,
-  }:
+    devenv,
+    fenix,
+  } @ inputs:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
-    in {
-      defaultPackage = pkgs.stdenv.mkDerivation {
-        pname = "bep.rs";
-        version = "0.1";
-        buildInputs = with pkgs; [cargo rustc git];
+    in rec {
+      devShells.default = devenv.lib.mkShell {
+        inherit inputs pkgs;
+        modules = [
+          ({
+            config,
+            pkgs,
+            ...
+          }: {
+            packages = with pkgs; [sqlite git diesel-cli openssl protobuf tokio-console openssl pkg-config];
+            languages.rust = {
+                enable = true;
+                version = "latest";
+            };
+            pre-commit.hooks = {
+              rustfmt.enable = true;
+            };
+          })
+        ];
       };
-      formatter = pkgs.alejandra;
     });
 }
+
+
+        #buildInputs = with pkgs; [cargo rustc git sqlite diesel-cli rust-analyzer protobuf rustfmt tokio-console clippy openssl pkg-config];
